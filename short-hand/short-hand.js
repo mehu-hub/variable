97aa8611f0c2804a1ee81071d9076bcbf3b75bbf
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 12:33:00 +0600

    Short Hand Operator

---
+++ short-hand/short-hand.js
@@ -0,0 +1,43 @@
+// variable declaration
+var orangePrice = 50;
+// variable update
+orangePrice = 80;
+
+// add something to the previous value
+var price1 = 35;
+var price2 = 50;
+
+// set a new value
+// price1 = 50;
+
+// add some value to existing value
+price1 = price1 + 10;
+// console.log(price1); 
+
+// addition to the same variable shorthand
+// += mean adding some value to the existing value
+price1 += 10;
+
+// price2 = price2 - 15;
+price2 -= 25;
+// console.log(price2);
+
+// multipication
+price1 *= 5;
+// console.log(price1);
+
+// division
+price1 /= 2;
+// console.log(price1);
+
+
+
+
+/* ------------------------------------
+-------Full Form--------    -------Short Hand----
+var price1 = price1 + 10    | var price1 += 10;
+var price1 = price1 - 10    | var price1 -= 10;
+var price1 = price1 * 10    | var price1 *= 10;
+var price1 = price1 / 10    | var price1 /= 10;
+
+*/ 
