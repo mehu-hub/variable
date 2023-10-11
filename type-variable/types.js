50daa160581eb7518fc4c47b481762627f4d413a
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 12:54:02 +0600

    Types of Variable

---
+++ type-variable/types.js
@@ -0,0 +1,12 @@
+var number = 25;
+console.log(typeof number);
+// ------------------------
+var isHappy = true;
+console.log(typeof isHappy);
+// ------------------------
+var name = 'Mehedi Hasan';
+console.log(typeof name);
+
+// Undefind --------------
+var technology;
+console.log(typeof technology);
