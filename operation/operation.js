1389471edbd0d5e18322e6796c0d9c3efcc7aac3
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 12:16:59 +0600

    Simple Math Operation

---
+++ operation/operation.js
@@ -0,0 +1,34 @@
+// Simple Math Operation
+var onionPrice = 30;
+var eggPrice = 20;
+
+// Addition, Substraction
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+// console.log(totalPrice);
+// console.log(priceDifference);
+// --------------------------------------
+  
+// Multipication
+var orangePrice = 50;
+var quantity = 25;
+
+var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+// --------------------------------------
+
+// Divison 
+var totalStudent = 100;
+var orange = 50;
+
+var perPersonGet = totalStudent / orange;
+// console.log(perPersonGet);
+
+var money = 500;
+var player = 10;
+
+var eachOne = money / player;
+// console.log(eachOne);
+// --------------------------------------
+
+
