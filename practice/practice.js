4c5c71ea28fb217cdcc3b6a24a15b50243d2d319
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 13:28:41 +0600

    Practice Task

---
+++ practice/practice.js
@@ -0,0 +1,32 @@
+// Practice problem - 1
+var momGives = 1000;
+var orangePrice = 400;
+var applePrice = 300;
+var totalPrice = orangePrice + applePrice;
+var dueHave = momGives - totalPrice;
+// console.log(dueHave);
+// --------------------
+
+// Practice problem - 2
+var bangla = 75.25; 
+var physics = 65;
+var biology = 80;
+var math = 35.45;
+var chemisty = 99.50;
+var totalMarks = bangla + physics + biology + math + chemisty; 
+var avgMarks = totalMarks / 5; 
+// console.log(avgMarks.toFixed(2));
+// ---------------------
+
+// Practice problem - 3
+var going = "I am going to be";
+var target = "an awesome web developer";
+var goal = going + ' ' + target;
+// console.log(goal);
+// ---------------------
+
+// practice problem - 4
+var marks = 119;
+var modulas = 5;
+var due = 119 % 5;
+// console.log(due);
