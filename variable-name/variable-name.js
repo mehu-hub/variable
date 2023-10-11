3fff8e813ba5c189a36c14fe4440f80ad42a0e71
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 11:48:36 +0600

    first commit

---
+++ variable-name/variable-name.js
@@ -0,0 +1,28 @@
+// Good Variable
+var price = 100;
+
+// 1. Variable name can not be any keywords
+var false = 96; 
+var return = true;
+
+// 2. variable name has to be in one word. no space
+var my home address = 'Feni, Bangladesh';
+
+// 3. variable name can not have quotation
+var "name" = "Mehedi Hasan";
+
+// 4. variable name can't starts with a number but can ends with a number
+var 99club = 1694;
+// right way 
+var club99 = 1694;
+
+// 5. How to use long names in variable
+// can not use dash
+var user-name = "Mehedi Hasan";
+var user-home-address = "Feni";
+// right way 
+var user_name = "Mehedi Hasan"
+var userHomeAddress = 'Feni';; 
+
+
+
