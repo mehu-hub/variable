17315d594a7b6eed76bbb416a79daec11183b361
Mehedi Hasan <designwithmehedi@gmail.com>
2023-10-11 12:58:53 +0600

    toFixed and parseFloat

--- type-variable/types.js
+++ type-variable/types.js
@@ -1,12 +1,21 @@
 var number = 25;
-console.log(typeof number);
+// console.log(typeof number);
 // ------------------------
 var isHappy = true;
-console.log(typeof isHappy);
+// console.log(typeof isHappy);
 // ------------------------
 var name = 'Mehedi Hasan';
-console.log(typeof name);
+// console.log(typeof name);
 
 // Undefind --------------
 var technology;
-console.log(typeof technology);
+// console.log(typeof technology);
+
+// -----------------------
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 * num2;
+var sum = sum.toFixed(2);
+sum = parseFloat(sum);
+console.log(sum);
